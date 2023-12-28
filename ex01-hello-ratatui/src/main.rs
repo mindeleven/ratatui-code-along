42dfb0666f2f9f0ac740d658e2dc29db2c5ba7df
mindeleven <mindeleven@posteo.de>
2023-12-28 15:32:55 +0100

    Adding crossterm and ratatui as dependencies

--- ex01-hello-ratatui/src/main.rs
+++ ex01-hello-ratatui/src/main.rs
@@ -1,3 +1,6 @@
+/// coding along with the tutorials at https://ratatui.rs/
+/// Example 01, Hello world @ https://ratatui.rs/tutorials/hello-world/
+/// 
 fn main() {
     println!("Hello, world!");
 }
