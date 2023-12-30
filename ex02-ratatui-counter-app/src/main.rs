12bfe64d2f187ef99a2b3a15eb30237e613fc8f9
mindeleven <mindeleven@posteo.de>
2023-12-30 09:13:37 +0100

    New project for coding along with the tutorials at https://ratatui.rs/, example 02, Counter App

---
+++ ex02-ratatui-counter-app/src/main.rs
@@ -0,0 +1,12 @@
+/// coding along with the tutorials at https://ratatui.rs/
+/// Example 02, Counter App @ https://ratatui.rs/tutorials/counter-app/
+/// 
+/// Create a TUI application
+/// the goal of this demonstration is to build a simple counter application
+/// the counter should be increment or decrement when a key is pressed
+/// (j for increment and k for decrement)
+/// 
+
+fn main() {
+    println!("Hello, world!");
+}
