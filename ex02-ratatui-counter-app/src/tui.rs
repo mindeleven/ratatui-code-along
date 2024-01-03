c7e390a2334c63a11ee29d1c10979b7e1aa6ec39
mindeleven <mindeleven@posteo.de>
2024-01-03 14:00:17 +0100

    Added functionality to initialize and clean up a terminal to tui module

--- ex02-ratatui-counter-app/src/tui.rs
+++ ex02-ratatui-counter-app/src/tui.rs
@@ -8,3 +8,17 @@ use crossterm::{
     ExecutableCommand,
 };
 use ratatui::prelude::*;
+
+// functuinality to initialize a terminal
+pub fn init() -> io::Result<Terminal<impl Backend>> {
+    stdout().execute(EnterAlternateScreen)?;
+    enable_raw_mode()?;
+    Terminal::new(CrosstermBackend::new(stdout()))
+}
+
+// functuinality to clean up a terminal
+pub fn restore() -> io::Result<()> {
+    stdout().execute(LeaveAlternateScreen)?;
+    disable_raw_mode()?;
+    Ok(())
+}
