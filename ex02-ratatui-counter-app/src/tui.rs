d73d930f0a0a9504c8862fe9146cf7d56c9d530a
mindeleven <mindeleven@posteo.de>
2024-01-03 13:24:34 +0100

    Importing the necessary components into tui.rs

---
+++ ex02-ratatui-counter-app/src/tui.rs
@@ -0,0 +1,10 @@
+/// importing the necessary components
+use std::io::{self, stdout};
+use crossterm::{
+    terminal::{
+        disable_raw_mode, enable_raw_mode, EnterAlternateScreen,
+        LeaveAlternateScreen,
+    },
+    ExecutableCommand,
+};
+use ratatui::prelude::*;
